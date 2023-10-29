9f289070d1af69c492883ba7e8ad13ac49bcc025
Hannapham1007 <hannaphamm1007@gmail.com>
2023-10-29 12:12:05 +0100

    improve UI

--- src/components/QuestionRadioButton.jsx
+++ src/components/QuestionRadioButton.jsx
@@ -7,7 +7,7 @@ function QuestionRadioButton({questionData, updateFunction}) {
       <p>{questionData.text}</p>
         {questionData.answers.map((answer, idx) => {
             return(
-              <div key={idx}>
+              <div key={idx} style={{marginBottom:'8px'}}>
                 <input type={questionData.type} name="radiobutton" onChange={updateFunction} value={answer.weighting}/>
                 <label htmlFor={`option${idx}`}>{answer.text}</label>
               </div>
--- src/components/QuestionSelectDropdown.jsx
+++ src/components/QuestionSelectDropdown.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 
 function QuestionSelectDropdown({questionData, updateFunction}) {
-  const dropdownStyle = { border: "5px solid #ffffff", borderRadius: "5px" };
+  const dropdownStyle = { border: "5px solid #ffffff", borderRadius: "5px", padding:'4px 12px' };
   const noWeighting = [0,0,0,0];
 
   return (
-    <div>
+    <div style={{textAlign:'center'}}>
       <p>{questionData.text}</p>
       <select style={dropdownStyle} onChange={updateFunction}>
         <option value={noWeighting}>Select .......</option>
